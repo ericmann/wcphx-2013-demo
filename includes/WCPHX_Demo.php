3f61fc485b2d1c1ddf12d91c38d8e3819eff060d
Eric Mann <eric@eamann.com>
2013-01-17 19:35:40 -0800

    Add Data Caching
    Rather than querying the database every time, let's cache the data in a transient.

--- includes/WCPHX_Demo.php
+++ includes/WCPHX_Demo.php
@@ -7,19 +7,31 @@ class WCPHX_Demo {
 	 * Grab a post from the database at random.
 	 *
 	 * @uses get_posts()
+	 * @uses wp_cache_get()
+	 * @uses wp_cache_set()
 	 *
 	 * @return object
 	 */
 	public function get_post_of_the_day() {
-		$args = array(
-			'numberposts' => 1,      // Select only one post
-			'order_by'    => 'rand', // Select the post at random
-			'post_type'   => 'post'  // Only request regular posts
-		);
+		// Attempt to fetch the post of the day from a transient.
+		$post = get_transient( 'wcphx2013_post_of_the_day' );
 
-		$posts = get_posts( $args );
+		if ( false === $post ) {
+			$args = array(
+				'numberposts' => 1,      // Select only one post
+				'order_by'    => 'rand', // Select the post at random
+				'post_type'   => 'post'  // Only request regular posts
+			);
 
-		// Return the first post in the array
-		return $posts[0];
+			$posts = get_posts( $args );
+
+			// Return the first post in the array
+			$post = $posts[0];
+
+			// Store the result in a transient for 24 hours so that we don't keep querying
+			set_transient( 'wcphx2013_post_of_the_day', $post, 60 * 60 * 24 );
+		}
+
+		return $post;
 	}
 }
